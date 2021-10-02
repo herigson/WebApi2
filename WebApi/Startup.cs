ef6b322064174973592ae345e50f0de635e73eb1
herigson <felipevbraga@outlook.com>
2021-10-01 23:22:19 -0300

    Utilizando Owin e Corrigindo swagger com owin.

---
+++ WebApi/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Swashbuckle.Application;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Xml.XPath;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.EnableSwagger(c => {
+                c.SingleApiVersion("v1", "WebApi");
+                c.IncludeXmlComments(AppDomain.CurrentDomain.BaseDirectory + @"\bin\WebApi.xml");
+            });
+            app.UseCors(CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+        }
+    }
+}
